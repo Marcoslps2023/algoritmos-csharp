85eacc045d3834f8089b7afac5522b60569b577f
Marcos <marcoslopesbrito7@gmail.com>
2026-04-13 07:18:22 -0300

    Busca Binaria em algoritmos

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Algoritmos.Busca;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 5, 12, 18, 23, 45, 70, 89 };
+        int alvo = 45;
+
+        int resultado = BuscaBinaria.Executar(numeros, alvo);
+
+        Console.WriteLine(resultado != -1
+            ? $"Achei no índice {resultado}"
+            : "Não achei");
+    }
+}
---
+++ src/Algoritmos/Busca/BuscaBinaria.cs
@@ -0,0 +1,26 @@
+﻿namespace Algoritmos.Busca
+{
+    public static class BuscaBinaria
+    {
+        public static int Executar(int[] arr, int alvo)
+        {
+            int baixo = 0;
+            int alto = arr.Length - 1;
+
+            while (baixo <= alto)
+            {
+                int meio = (baixo + alto) / 2;
+
+                if (arr[meio] == alvo)
+                    return meio;
+
+                if (arr[meio] < alvo)
+                    baixo = meio + 1;
+                else
+                    alto = meio - 1;
+            }
+
+            return -1;
+        }
+    }
+}
